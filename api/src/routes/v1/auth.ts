9db794ee853ba16286d75b9bd2c3247c2522c3c5
mikeru <127611876+mikeruC@users.noreply.github.com>
2025-10-02 20:26:53 +0800

    added undeclared routes in user and auth folders

--- api/src/routes/v1/auth.ts
+++ api/src/routes/v1/auth.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Request, Response, Router } from 'express';
 import SignUp from '../../controllers/auth/SignUp';
 import SignIn from '../../controllers/auth/SignIn';
+import { Login, Logout, Profile, RefreshToken, Register } from '../../controllers/auth/Auth';
+import { profile } from 'console';
 
 const AuthRouter = Router();
 
@@ -9,5 +11,10 @@ const AuthRouter = Router();
 // refresh token, generate/Verify OTP, e.t.c
 AuthRouter.post('/signup', SignUp);
 AuthRouter.post('/signin', SignIn);
+AuthRouter.post('/login', Login);
+AuthRouter.post('/register', Register);
+AuthRouter.post('/logout', Logout);
+AuthRouter.post('/refreshToken', RefreshToken);
+AuthRouter.post('/profile', Profile);
 
 export default AuthRouter;
