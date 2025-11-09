35c95f35c16ab7cc8f7e8add96d549c09e4ecee0
Genrey <109770981+Genrei123@users.noreply.github.com>
2025-11-09 18:19:19 +0800

    Fixed lint error in main

--- api/src/utils/ProductChainUtil.ts
+++ api/src/utils/ProductChainUtil.ts
@@ -1,28 +1,28 @@
-import { globalProductBlockchain } from "../controllers/scan/Scan"
+// import { globalProductBlockchain } from "../controllers/scan/Scan"
 import { DB } from "../typeorm/data-source";
 
 export const searchProductInBlockchain = (searchTerm: string) => {
     // Blockchain must already be implemented
-    if (!globalProductBlockchain) {
-        return null;
-    }
+    // if (!globalProductBlockchain) {
+    //     return null;
+    // }
 
-    const blockchain = globalProductBlockchain.blockhain;
-    for (let i = 0; i < blockchain.length; i++) {
-        const block = blockchain[i];
-        const product = block.data.productName;
+    // const blockchain = globalProductBlockchain.blockhain;
+    // for (let i = 0; i < blockchain.length; i++) {
+    //     const block = blockchain[i];
+    //     const product = block.data.productName;
 
-        if (product.toLowerCase() === searchTerm.toLowerCase()) {
-            return block;
-        }
-    }
+    //     if (product.toLowerCase() === searchTerm.toLowerCase()) {
+    //         return block;
+    //     }
+    // }
 
-    return null;
+    // return null;
 }
 
 export const getAllProductsInBlockchain = () => {
-    if (!globalProductBlockchain) {
-        return [];
-    }
-    return globalProductBlockchain.blockhain;
+    // if (!globalProductBlockchain) {
+    //     return [];
+    // }
+    // return globalProductBlockchain.blockhain;
 }
