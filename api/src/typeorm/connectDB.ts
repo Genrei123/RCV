9b07964fe1b128563447b2c795964ad1497dd13c
Genrey <genreycristobal99@gmail.com>
2025-12-26 02:28:23 +0800

    Added account initializer

--- api/src/typeorm/connectDB.ts
+++ api/src/typeorm/connectDB.ts
@@ -1,4 +1,5 @@
 import { DB } from './data-source';
+import { User } from './entities/user.entity';
 
 /**
  * Connects to the database and initializes the data source.
@@ -13,6 +14,27 @@ const ConnectDatabase = async (): Promise<void> => {
   try {
     await DB.initialize();
     console.log('Data Source has been initialized!');
+
+    // First account seeder
+    const userRepository = DB.getRepository(User);
+    const dummyUser = userRepository.create({
+      firstName: 'Admin',
+      lastName: 'User',
+      fullName: 'Admin User',
+      email: 'adminuser@gmail.com',
+      location: 'Admin City',
+      dateOfBirth: '1990-01-01',
+      phoneNumber: '1234567890',
+      role: 'ADMIN',
+      status: 'Active',
+      approved: true,
+      emailVerified: true,
+    });
+    const existingUser = await userRepository.findOneBy({ email: dummyUser.email });
+    if (!existingUser) {
+      await userRepository.save(dummyUser);
+      console.log('Admin user created with email');
+    }
   } catch (error) {
     console.error('Error during Data Source initialization:', error);
     process.exit(1);
