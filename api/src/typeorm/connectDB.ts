b0b886ed76c50208eb34334bc982f88a729c3b3b
Genrey <genreycristobal99@gmail.com>
2025-12-26 02:29:36 +0800

    Removed account initializer

--- api/src/typeorm/connectDB.ts
+++ api/src/typeorm/connectDB.ts
@@ -14,28 +14,6 @@ const ConnectDatabase = async (): Promise<void> => {
   try {
     await DB.initialize();
     console.log('Data Source has been initialized!');
-
-    // First account seeder
-    // REMOVE IN PRODUCTION
-    const userRepository = DB.getRepository(User);
-    const dummyUser = userRepository.create({
-      firstName: 'Admin',
-      lastName: 'User',
-      fullName: 'Admin User',
-      email: 'adminuser@gmail.com',
-      location: 'Admin City',
-      dateOfBirth: '1990-01-01',
-      phoneNumber: '1234567890',
-      role: 'ADMIN',
-      status: 'Active',
-      approved: true,
-      emailVerified: true,
-    });
-    const existingUser = await userRepository.findOneBy({ email: dummyUser.email });
-    if (!existingUser) {
-      await userRepository.save(dummyUser);
-      console.log('Admin user created with email');
-    }
   } catch (error) {
     console.error('Error during Data Source initialization:', error);
     process.exit(1);
