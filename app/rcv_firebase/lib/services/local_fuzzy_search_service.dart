7622b8f6d60cf4c7d5e15c4a0717e0f370cebd8a
Zyciann 01 <gianhiginofungo@gmail.com>
2026-03-09 23:20:31 +0800

    fixes the bugs

--- app/rcv_firebase/lib/services/local_fuzzy_search_service.dart
+++ app/rcv_firebase/lib/services/local_fuzzy_search_service.dart
@@ -199,8 +199,12 @@ class LocalFuzzySearchService {
     final m = s1.length;
     final n = s2.length;
     final dp = List.generate(m + 1, (_) => List.filled(n + 1, 0));
-    for (var i = 0; i <= m; i++) dp[i][0] = i;
-    for (var j = 0; j <= n; j++) dp[0][j] = j;
+    for (var i = 0; i <= m; i++) {
+      dp[i][0] = i;
+    }
+    for (var j = 0; j <= n; j++) {
+      dp[0][j] = j;
+    }
     for (var i = 1; i <= m; i++) {
       for (var j = 1; j <= n; j++) {
         if (s1[i - 1] == s2[j - 1]) {
